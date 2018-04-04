f49ab36dddc60581e4229246e9da358d96a5e560
csy9608 <ysc9608@gmail.com>
2018-04-04 17:59:56 +0900

    180404_csy

--- nodejs_codes/mysql1.js
+++ nodejs_codes/mysql1.js
@@ -29,35 +29,28 @@ let refine_recipe = (ugly_recipe) => {
 }
 
 let recommend_recipe = (context) => {
-  var ingredients = context.ingredient;
-  var query = "SELECT * FROM RECIPES WHERE ingredients LIKE '%/"+ingredients + "/%';";
-  con.query(query, function(err, result){
-    if(err)
-      console.log("error ! :" + err);
-    else{
-  //    context.output = JSON.stringify(result);
-      return context;
-    }
-  });
+  context.command = undefined;
+  context.need_conversation = true;
+
   return new Promise((resolved, rejected) => {
 
-  var query = ;
-  console.log(query);
-  con.query(query , function(err, result) {
-    if(err)
-      console.log(query);
-    else{
-      if(context.data == undefined){
-        context.data = {};
-      }
+    var ingredients = context.data.ingredients;
+    var menu_type = context.data.menu_type;
+    var query = "SELECT menu FROM " + menu_type + " WHERE ingredient LIKE '%"+ingredients + "%';";
+    console.log(query);
+
+    con.query(query , function(err, result) {
+      if(err)
+        console.log("error ! :" + err);
       else{
-
+        var menus = [];
+        JSON.parse(JSON.stringify(result), (u,v) => {
+          menus[u] = v;
+        });
+        context.data.recom_menu_list = JSON.stringify(result) || {};
+        console.log(context.data.recom_menu_list);
+        resolved(context);
       }
-
-      context.data.recipe_result = refine_recipe(result) || {};
-      console.log(context.data.recipe_result);
-      resolved(context);
-    }
   });
   });
 }
@@ -74,17 +67,153 @@ let search_recipe = (context) => {
         if(err)
           console.log("error ! :" + err);
         else{
-          context.data = {};
           context.data.recipe_result = refine_recipe(result) || {};
           console.log(context.data.recipe_result);
           resolved(context);
         }
       });
     });
-};
+}
 
+
+let check_id = (context) => {
+  context.command = undefined;
+  context.need_conversation = true;
+
+  return new Promise((resolved, rejected) => {
+    var user_id = context.data.user_id;
+    var query = "SELECT user_id FROM users WHERE user_id='" + user_id + "';";
+    console.log(query);
+
+    con.query(query, function(err, result) {
+      if(err)
+        console.log("error ! :" + err);
+      else{
+        var pretty_result = [];
+        JSON.parse(JSON.stringify(result), (u,v) => {
+          pretty_result[u] = v;
+        });
+        if(typeof pretty_result['user_id'] === 'undefined'){
+          context.data.id_exists = false;
+          var query = "INSERT INTO users(user_id) VALUES('" + user_id + "');";
+          console.log(query);
+
+          con.query(query, function(err, result) {
+            if(err)
+              console.log("error !" + err);
+            else{
+              context.login = true;
+              console.log(context.data.id_exists);
+              resolved(context);
+            }
+          });
+        }
+        else{
+          context.data.id_exists = true;
+          context.data.user_id = undefined;
+          console.log(context.data.id_exists);
+          resolved(context);
+        }
+      }
+    });
+  });
+}
+
+let login = (context) => {
+  context.command = undefined;
+  context.need_conversation = true;
+
+  return new Promise((resolved, rejected) => {
+    var user_id = context.data.user_id;
+    var query = "SELECT * FROM users WHERE user_id='" + user_id + "';";
+    console.log(query);
+    con.query(query, function(err, result){
+      if(err)
+        console.log("error ! :" + err);
+      else{
+        var pretty_result = [];
+        JSON.parse(JSON.stringify(result), (u,v) => {
+          pretty_result[u] = v;
+        });
+
+        if(typeof pretty_result['user_id'] === undefined){
+          context.login = false;
+          context.data.user_id = undefined;
+        }
+        else{
+          context.login = true;
+          context.data.preference = {};
+          context.data.preference.allergy = pretty_result['allergy'];
+          context.data.preference.priority = pretty_result['priority'];
+          context.data.preference.likes = pretty_result['likes'];
+          context.data.preference.hates = pretty_result['hates'];
+        }
+        console.log(context.data.login);
+        resolved(context);
+      }
+    });
+  });
+}
+
+let user_settings = (context) => {
+  context.command = undefined;
+  context.need_conversation = true;
+
+  return new Promise((resolved, rejected) => {
+    var user_id = context.data.user_id;
+    var allergy = context.data.preference.allergy;
+    var priority = context.data.preference.priority;
+    var likes = context.data.preference.likes;
+    var hates = context.data.preference.hates;
+    var settings = []
+
+    if(context.login){
+          if (typeof allergy !== undefined){
+              settings.push({col:'allergy', val:allergy});
+          }
+
+          if (typeof priority !== undefined){
+              settings.push({col:'priority', val:priority});
+          }
+
+          if (typeof likes !== undefined){
+              settings.push({col:'likes', val:likes});
+          }
+
+          if (typeof hates !== undefined){
+              settings.push({col:'hates', val:hates});
+          }
+
+          var query = "UPDATE users SET ";
+          for(var j=0 ; j<settings.length; j++){
+
+            sub_query = settings[j]['col'] + "= '" + settings[j]['val'] + "'";
+            if (j!=(settings.length-1))
+              sub_query += ",";
+
+            query += sub_query;
+          }
+          query += " WHERE user_id='" + user_id + "';"
+          console.log(query);
+
+          con.query(query, function(err, result){
+            if(err)
+              console.log("error ! :" + err);
+            else{
+              resolved(context);
+            }
+          });
+    }
+    else {
+      resolved(context);
+    }
+  });
+}
 
 module.exports = {
     'recommend_recipe': recommend_recipe,
-    'search_recipe' : search_recipe
+    'search_recipe' : search_recipe,
+    'check_id' : check_id,
+    'login' : login,
+    "user_settings" : user_settings
 };
